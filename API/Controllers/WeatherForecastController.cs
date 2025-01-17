f53e9d5a6cef8b9d0b67c0e460edd858f6029e06
HoangBaoAES <hoangbao.nguyen@asiaesolutions.com>
2025-01-17 17:37:22 +0700

    ,

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -22,18 +22,6 @@ namespace API.Controllers
             _responseCacheService = responseCacheService;
         }
 
-        // [HttpGet]
-        // [Cache(100)]
-        // public IEnumerable<WeatherForecast> Get()
-        // {
-        //     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        //     {
-        //         Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-        //         TemperatureC = Random.Shared.Next(-20, 55),
-        //         Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        //     })
-        //     .ToArray();
-        // }
         [HttpGet("getall")]
         [Cache(100)]
         public async Task<IActionResult> Get()
